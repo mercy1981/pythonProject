852c29e6572771f267d9a7879854a46d1999faa6
mercymagdhalena@gmail.com <mercymagdhalena@gmail.com>
2024-03-12 20:03:43 +0530

    FirstPythonProject

---
+++ whileloop.py
@@ -0,0 +1,11 @@
+while True:
+    user_input=input("Enter user input to exit")
+    print(user_input)
+    if user_input == "5":
+        break
+
+i=0
+while i<5:
+    print(i)
+    i=i+1
+print("End")
